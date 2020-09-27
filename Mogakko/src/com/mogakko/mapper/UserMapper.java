d553d9624c419be6ed5bc4c74e71ff0f48c1c623
JG HAN <60098769+JongGurlHan@users.noreply.github.com>
2020-09-27 21:56:53 +0900

    9.27 호스팅설정 완료

--- Mogakko/src/com/mogakko/mapper/UserMapper.java
+++ Mogakko/src/com/mogakko/mapper/UserMapper.java
@@ -8,20 +8,20 @@ import com.mogakko.beans.UserBean;
 
 public interface UserMapper {
 	
-	@Select("SELECT USER_NAME FROM USER_TABLE WHERE USER_ID = #{user_id}")
+	@Select("SELECT user_name FROM gojkhan13.user_table WHERE user_id = #{user_id}")
 	String checkUserIdExist(String user_id);
 	
-	@Insert("INSERT INTO USER_TABLE(USER_NAME, USER_ID, USER_PW)"+
+	@Insert("INSERT INTO gojkhan13.user_table(user_name, user_id, user_pw)"+
 			"VALUES (#{user_name}, #{user_id}, #{user_pw})")
 	void addUserInfo(UserBean joinUserBean);
 	
-	@Select("SELECT USER_IDX, USER_NAME FROM USER_TABLE WHERE user_id = #{user_id} and user_pw = #{user_pw}")
+	@Select("SELECT user_idx, user_name FROM gojkhan13.user_table WHERE user_id = #{user_id} and user_pw = #{user_pw}")
 	UserBean getLoginUserInfo(UserBean tempLoginUserBean);
 	
 	//현재 로그인한 사용자 인덱스 번호로
-	@Select("select user_id, user_name from user_table where user_idx = #{user_idx}")
+	@Select("select user_id, user_name from gojkhan13.user_table where user_idx = #{user_idx}")
 	UserBean getModifyUserInfo(int user_idx);
 
-	@Update("update user_table set user_pw = #{user_pw} where user_idx = #{user_idx}")
+	@Update("update gojkhan13.user_table set user_pw = #{user_pw} where user_idx = #{user_idx}")
 	void modifyUserInfo(UserBean modifyUserBean);
 }
